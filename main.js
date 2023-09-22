c11c7a1a7909b09112d562b1bcc205ace6cf46d5
ywonchae1 <ywonchae62@gmail.com>
2023-09-22 18:12:19 +0900

    #1 feat: cors 허용

--- main.js
+++ main.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+//cors 설정
+const cors = require('cors');
+// const corsOptions = {
+//   origin: 'http://localhost',
+//   credentials: true,
+//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+// }
+// app.use(cors(corsOptions));
+app.use(cors());
+
 const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
