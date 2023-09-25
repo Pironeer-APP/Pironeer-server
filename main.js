fa1a5031ec10959664058899be2d0a8eb11328ac
minseo jang <mingdo.dev@gmail.com>
2023-09-26 00:27:39 +0900

    style: main.js 과제 라우터 코드 정리

--- main.js
+++ main.js
@@ -21,12 +21,10 @@ app.use(bodyParser.json());
 const authRouter = require('./routers/authRouter.js');
 const adminRouter = require('./routers/adminRouter.js');
 const postRouter = require('./routers/postRouter.js');
+const assignRouter = require('./routers/assignRouter.js');
 app.use('/api/auth', authRouter);
 app.use('/api/admin', adminRouter);
 app.use('/api/post', postRouter);
-
-// 과제 페이지
-const assignRouter = require('./routers/assignRouter.js');
 app.use('/api/assign', assignRouter);
 
 app.listen(port, () => {
