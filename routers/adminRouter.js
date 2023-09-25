ff17381066b1554e3b7b9c88f1f6008d7b0bdc64
ywonchae1 <ywonchae62@gmail.com>
2023-09-26 02:06:47 +0900

    #8 feat: 보증금 차감 내역 조회, attend와 assign union 적용

--- routers/adminRouter.js
+++ routers/adminRouter.js
@@ -4,5 +4,8 @@ const adminController = require('../controllers/adminController.js');
 
 router.post('/getUserInfo', adminController.getUserInfo);
 router.post('/addCoupon', adminController.addCouponToUser);
+router.post('/getDepositHistory', adminController.getDepositHistory);
+router.post('/getCoupons', adminController.getCoupons);
+router.post('/getOneUserInfo', adminController.getOneUserInfo);
 
 module.exports = router;
