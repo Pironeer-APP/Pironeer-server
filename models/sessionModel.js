e0c7dd3bf7b857eaee1815dfac15ea342e990412
ywonchae1 <ywonchae62@gmail.com>
2023-09-30 23:40:12 +0900

    #28 feat: 관리자 세션 일정 등록 기능 구현

--- models/sessionModel.js
+++ models/sessionModel.js
@@ -1,7 +1,25 @@
 const db = require('../config/db.js');
 
 module.exports = {
-  addSchedule: async (schedule) => {
-    //세원
+  addSchedule: async (title, date, face, place, level) => {
+    const query = `
+    INSERT
+    INTO Session(level, title, location, date, is_face)
+    VALUES(?, ?, ?, ?, ?);`;
+    const result = await db.query(query, [level, title, place, date, face]);
+
+    return result[0]
+  },
+  deleteSchedule: async (session_id) => {
+    const query = 'DELETE FROM Session WHERE session_id=?;';
+    const result = await db.query(query, [session_id]);
+
+    return result[0];
+  },
+  getSessions: async (level) => {
+    const query = 'SELECT * FROM Session WHERE level=? ORDER BY date desc;';
+    const sessions = await db.query(query, [level]);
+    
+    return sessions[0];
   }
 }
--- routers/sessionRouter.js
+++ routers/sessionRouter.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const sessionController = require('../controllers/sessionController.js');
 
 router.post('/addSchedule', sessionController.addSchedule);
+router.post('/deleteSchedule', sessionController.deleteSchedule);
+router.post('/getSessions', sessionController.getSessions);
 
 module.exports = router;
