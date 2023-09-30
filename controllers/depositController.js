e544e66c5caf6d7da1e62b592291ec87eab6bb47
Jeonggon Kim <kjjkjykjk@gmail.com>
2023-09-30 21:26:30 +0900

    #33 [add] createCoupon function 추가

--- controllers/depositController.js
+++ controllers/depositController.js
@@ -4,20 +4,16 @@ const jwt = require('jsonwebtoken');
 module.exports = {
   getDepositHistory: async (req, res) => {
     const userInfo = req.body.userInfo;
-    console.log(1);
-    console.log(req.body)
-    console.log(userInfo.user_id)
-    console.log ('1123123'+userInfo);
     const histories = await depositModel.getDepositHistory(userInfo);
-    console.log(histories);
-    console.log(3)
     res.json({histories: histories});
   },
   getCoupons: async (req, res) => {
     const userInfo = req.body.userInfo;
     const couponInfo = await depositModel.getCoupons(userInfo);
-
-    console.log(couponInfo);
     res.json({couponInfo: couponInfo});
   },
+  createCoupon: async (req,res) => {
+    const userInfo = req.body.userInfo;
+    await depositModel.createCoupon(userInfo);
+  }
 }
