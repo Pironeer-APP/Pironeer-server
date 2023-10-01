a0d7c9490ef97ab3fef0fb70bf6d357bfc1abc11
hwanheejung <hwanheejung117@gmail.com>
2023-10-01 23:04:35 +0900

    #51 feat: 과제 create, read server 연결 완료

--- models/assignModel.js
+++ models/assignModel.js
@@ -1,8 +1,8 @@
-const db = require('../config/db.js');
+const db = require("../config/db.js");
 
 module.exports = {
-    showAssign: async (curUserLevel, curUserId) => {
-        const query = `
+  showAssign: async (curUserLevel, curUserId) => {
+    const query = `
         SELECT
          ROW_NUMBER() OVER (ORDER BY AssignSchedule.created_at) AS AssignId,
          AssignSchedule.title, AssignSchedule.due_date, AssignSchedule.created_at,
@@ -20,27 +20,30 @@ module.exports = {
         WHERE
          AssignSchedule.level = ? AND Assign.user_id = ?;
         `;
-        const AssignData = await db.query(query, [curUserLevel, curUserId]); // 회원의 기수, 회원 식별자로 필터링
-        const Assign = AssignData[0];
+    const AssignData = await db.query(query, [curUserLevel, curUserId]); // 회원의 기수, 회원 식별자로 필터링
+    const Assign = AssignData[0];
 
-        return Assign;
-    },
-    readAssignAll: async (level) => {
-        const query = `
+    return Assign;
+  },
+  readAssignAll: async (level) => {
+    const query = `
         SELECT
-         title, created_at,assignschedule_id
+         title, created_at,assignschedule_id, due_date
         FROM
          AssignSchedule
         WHERE
-         level = ?;
-        `
-        const AllData = await db.query(query, [level]);
-        const Data = AllData[0];
-
-        return Data;
-    },
-    readAssignDetail: async (title, level) => {
-        const query = `
+         level = ?
+        ORDER BY 
+         due_date 
+        DESC;
+        `;
+    const AllData = await db.query(query, [level]);
+    const Data = AllData[0];
+    console.log("Data: ", Data);
+    return Data;
+  },
+  readAssignDetail: async (title, level) => {
+    const query = `
         SELECT
          ROW_NUMBER() OVER (ORDER BY Assign.user_id) AS AssignUserId,
          AssignSchedule.title,
@@ -53,71 +56,71 @@ module.exports = {
          AssignSchedule.assignschedule_id = Assign.assignschedule_id
         WHERE
          AssignSchedule.title = ? AND AssignSchedule.level = ?;
-        `
-        const AssignDetailData = db.query(query, [title, level]);
-        const Data = AssignDetailData[0];
+        `;
+    const AssignDetailData = db.query(query, [title, level]);
+    const Data = AssignDetailData[0];
 
-        return Data;
-    },
-    createAssign: async (level, inputTitle, inputDueDate) => {
-        const query = `
+    return Data;
+  },
+  createAssign: async (level, inputTitle, inputDueDate) => {
+    const query = `
         INSERT INTO
          AssignSchedule (level, title, due_date) VALUES (?, ?, ?);
-        `
-        db.query(query, [level, inputTitle, inputDueDate]);
-    },
-    updateAssign: async (level, updateId) => {
-        const query = `
+        `;
+    db.query(query, [level, inputTitle, inputDueDate]);
+  },
+  updateAssign: async (level, updateId) => {
+    const query = `
         SELECT
          title, due_date
         FROM
          AssignSchedule
         WHERE
          level = ? AND assignschedule_id = ?;
-        `
-        const Data = db.query(query, [level, updateId])
+        `;
+    const Data = db.query(query, [level, updateId]);
 
-        return Data[0];
-    },
-    updatedAssign: async (level, updateId, newTitle, newDueDate) => {
-        const query = `
+    return Data[0];
+  },
+  updatedAssign: async (level, updateId, newTitle, newDueDate) => {
+    const query = `
         UPDATE
          AssignSchedule
         SET
          level = ? , title = ? , due_date = ?
         WHERE
          assignschedule_id = ?;
-        `
-        db.query(query, [level, updateId, newTitle, newDueDate]);
-    },
-    deleteAssign: async (level, deleteId) => {
-        const query = `
+        `;
+    db.query(query, [level, updateId, newTitle, newDueDate]);
+  },
+  deleteAssign: async (level, deleteId) => {
+    const query = `
         DELETE FROM
          AssignSchedule
         WHERE
          level = ? AND assignschedule_id = ?;
-        `
-        db.query(query, [level, deleteId]);
-    },
-    readGrade: async (assignScheduleId, UserId) => {
-        const query = `
+        `;
+    db.query(query, [level, deleteId]);
+  },
+  readGrade: async (assignScheduleId, UserId) => {
+    const query = `
         SELECT
          grade
         FROM
          Assign
         WHERE
          assignschedule_id = ? AND user_id = ?;
-        `
-        const data = db.query(query, [assignScheduleId, UserId]);
+        `;
+    const data = db.query(query, [assignScheduleId, UserId]);
 
-        return data[0];
-    },
-    createGrade: async (assignScheduleId, UserId, inputGrade, inputReason) => {
-        const query = `
+    return data[0];
+  },
+  createGrade: async (assignScheduleId, UserId, inputGrade, inputReason) => {
+    const query = `
         INSERT INTO
          Assign (user_id, grade, reason, assignschedule_id)
         VALUES (? , ? , ?, ?);
-        `
-        db.query(query, [UserId, inputGrade, inputReason, assignScheduleId]);
-    }
-}
+        `;
+    db.query(query, [UserId, inputGrade, inputReason, assignScheduleId]);
+  },
+};
