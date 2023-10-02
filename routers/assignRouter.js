8fbef0ef4c73a801d1b4d82a80dcf051e06ec3a3
minseo jang <mingdo.dev@gmail.com>
2023-10-02 22:50:17 +0900

    #52 fix: 과제 일정 세부 사항 url 수정

--- routers/assignRouter.js
+++ routers/assignRouter.js
@@ -4,8 +4,8 @@ const assignController = require('../controllers/assignController.js');
 
 router.post('/', assignController.showAssign);
 router.post("/readAssign/all", assignController.readAssignAll);
-// 여기서 AssignSchedule_id는 실제 값 X 마감 기한에 따른 정렬 순서 id
-router.post("/readAssign/:NewAssignId", assignController.readAssignDetail);
+// 여기서 NewAssignId는 실제 값 X 마감 기한에 따른 정렬 순서 id
+router.post("/readAssign/detail/:NewAssignId", assignController.readAssignDetail);
 router.post("/createAssign", assignController.createAssign);
 router.post("/updateAssign", assignController.updateAssign);
 router.post("/deleteAssign", assignController.deleteAssign);
