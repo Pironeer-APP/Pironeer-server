adc9d832d7fb5da1ecdd9dbe443dc2696d5f5074
ywonchae1 <ywonchae62@gmail.com>
2023-10-12 09:01:20 +0900

    #133 feat: mysql timezone 설정

--- config/db.js
+++ config/db.js
@@ -6,7 +6,8 @@ const db = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  database: process.env.DB_NAME
+  database: process.env.DB_NAME,
+  timezone: 'utc'
 });
 
 module.exports = db;
